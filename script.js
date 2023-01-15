26e7f1c71db2a0276d57e05c5c65cf7fdcd29e53
Zekseed <alan.barazanji@gmail.com>
2023-01-15 20:03:53 +0100

    Draft Seven
    Finished Work

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ fetch("./data.json").then((response) => {
 
 });
 
+const data = JSON.stringify(json);
+
 document.addEventListener("click", function(event) {
+    if (event.target.parentNode.tagName !== "LI" && document.getElementById("popup") !== null){
+        document.getElementById("popup").remove();
+    }
+
     if (event.target.parentNode.tagName == "LI"){
-        
+
         //for removing the popup window if it already exists and thus not overlapping
         if (document.getElementById("popup") !== null){
             document.getElementById("popup").remove();
@@ -16,63 +22,100 @@ document.addEventListener("click", function(event) {
 
         //for popup window
         const popup = document.createElement("div");
-
-        popup.style.backgroundColor = "rgb(45, 45, 45)";
-        popup.style.position = "fixed";
-        popup.style.height = "40vw";
-        popup.style.width = "90vw";
-        popup.style.top = "50%";
-        popup.style.left = "50%";
-        popup.style.transform = "translate(-50%, -50%)";
-        popup.style.zIndex = "1000000";
-        popup.style.borderRadius = "10px";
-        popup.style.padding = "10px";
-        popup.style.color = "white";
-        popup.style.display = "flex";
-        popup.style.flexFlow = "row wrap";
         popup.setAttribute("id", "popup");
 
         //for close button
         const close = document.createElement("img");
-
-        close.style.position = "absolute";
-        close.style.top = "0";
-        close.style.right = "0";
-        close.style.margin = "10px";
-        close.style.height = "20px";
-        close.style.width = "20px";
-        close.src = "./x.svg";
-        close.style.cursor = "pointer";
+        close.setAttribute("id", "close");
         close.setAttribute("onclick", "closeEvent()")
-
+        close.src = "./x.svg";
         popup.appendChild(close);
 
         //for items in popup window (important factor of 2.25)
         const popupItem = document.createElement("div");
         const element = getComputedStyle(event.target.parentNode);
         console.log(element.backgroundColor);
-        popupItem.id = "popupItem";
+        popupItem.className = "popupItem";
         popupItem.style.backgroundColor = element.backgroundColor;
 
-        popup.appendChild(popupItem);
+        const tempdiv = document.createElement("div");
+        const b = document.createElement("b");
+        const em = document.createElement("em");
+        const abb = document.createElement("abbr");
+        const strong = document.createElement("strong");
+        const data = document.createElement("data");
+
+        //for elements in popupItem
+        popupItem.appendChild(tempdiv.cloneNode(true));
+        popupItem.childNodes[0].appendChild(b.cloneNode(true));
+        popupItem.childNodes[0].appendChild(em.cloneNode(true));
+        popupItem.childNodes[0].className = "top"
+
+        popupItem.appendChild(abb.cloneNode(true));
+
+        popupItem.appendChild(tempdiv.cloneNode(true));
+        popupItem.childNodes[2].appendChild(strong.cloneNode(true));
+        popupItem.childNodes[2].appendChild(data.cloneNode(true));
+        popupItem.childNodes[2].className = "bottom"
 
         //check if the user clicked on the abbr tag since it has already checked if the user clicked on a child of an li element
         if (event.target.tagName !== "ABBR"){
             const abbr = event.target.parentNode.querySelector("abbr").innerText;
-            console.log(json.find((e) => abbr === e.symbol));
-            
+
+            for (var i = 0; i < json.length; i++) {
+                if (json[i].symbol === abbr) {
+
+                    var temp = popupItem.cloneNode(true);
+                    temp.querySelector("b").innerText = json[i].z;
+                    temp.querySelector("em").innerText = json[i].n;
+                    temp.querySelector("abbr").innerText = json[i].symbol;
+                    if (json[i].z !== null || json[i].n !== null){
+                        temp.querySelector("strong").innerText = json[i].z + json[i].n;
+                    }
+                    if (json[i].decay_2 !== "" && json[i].decay_3 !== ""){
+                        temp.querySelector("data").innerText = json[i].decay_1 + "\n" + json[i].decay_2 + "\n" + json[i].decay_3;
+                    }
+                    else if (json[i].decay_2 !== ""){
+                        temp.querySelector("data").innerText = json[i].decay_1 + "\n" + json[i].decay_2;
+                    }
+                    else {
+                        temp.querySelector("data").innerText = json[i].decay_1;
+                    }
+                    console.log(temp);
+                    popup.appendChild(temp);
+                }
+            }
+
             document.body.appendChild(popup);
         }
         //otherwise it would mean that the user clicked on the abbr tag
         else {
             const abbr = event.target.innerText;
-            console.log(json.find((e) => abbr === e.symbol));
-            
-            
 
-            document.body.appendChild(popup);
+            for (var i = 0; i < json.length; i++) {
+                if (json[i].symbol === abbr) {
+
+                    var temp = popupItem.cloneNode(true);
+                    temp.querySelector("b").innerText = json[i].z;
+                    temp.querySelector("em").innerText = json[i].n;
+                    temp.querySelector("abbr").innerText = json[i].symbol;
+                    if (json[i].z !== null || json[i].n !== null){
+                        temp.querySelector("strong").innerText = json[i].z + json[i].n;
+                    }
+                    if (json[i].decay_2 !== "" && json[i].decay_3 !== ""){
+                        temp.querySelector("data").innerText = json[i].decay_1 + "\n" + json[i].decay_2 + "\n" + json[i].decay_3;
+                    }
+                    else if (json[i].decay_2 !== ""){
+                        temp.querySelector("data").innerText = json[i].decay_1 + "\n" + json[i].decay_2;
+                    }
+                    else {
+                        temp.querySelector("data").innerText = json[i].decay_1;
+                    }
+                    console.log(temp);
+                    popup.appendChild(temp);
+                }
+            }
         }
-
     }
 });
 //for closing and animating the popup window
